c98f3b6596b6604ec5c7f20d9fc6f32d1c174354
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-04-24 21:36:25 -0400

    Guarda archivos 1

---
+++ Lab5Poo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5POO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
